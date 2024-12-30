69e758a25d3cf0d548be9e0165340a4fa6119480
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-30 14:27:31 -0300

    refactor: métodos da classe person array não lançam mais exceção

--- src/main/java/vitor/dev/array/MyArray.java
+++ src/main/java/vitor/dev/array/MyArray.java
@@ -3,71 +3,71 @@ package vitor.dev.array;
 import vitor.dev.model.Model;
 
 public class MyArray {
-    Model[] items;
+	Model[] items;
 
-    public MyArray(int size) {
-        this.items = new Model[size];
-    }
+	public MyArray(int size) {
+		this.items = new Model[size];
+	}
 
-    public Model[] getItems() {
-        return items;
-    }
+	public Model[] getItems() {
+		return items;
+	}
 
-    public void insert(int position, Model model) {
-        items[position] = model;
-    }
+	public void insert(int position, Model model) {
+		items[position] = model;
+	}
 
-    public boolean remove(Model model) {
-        for (int i = 0; i < items.length; i++) {
-            if (items[i] == model) {
-                items[i] = null;
-                return true;
-            }
-        }
+	public boolean remove(Model model) {
+		for (int i = 0; i < items.length; i++) {
+			if (items[i] == model) {
+				items[i] = null;
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public boolean search(Model model) {
-        for (Model item : items) {
-            if (item == model) {
-                return true;
-            }
-        }
+	public boolean search(Model model) {
+		for (Model item : items) {
+			if (item == model) {
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public void orderByIdAsc() {
-        for (int i = 0; i < items.length - 1; i++) {
-            for (int j = 0; j < items.length - i - 1; j++) {
-                if (items[j].getId() > items[j + 1].getId()) {
-                    Model temp = items[j];
-                    items[j] = items[j + 1];
-                    items[j + 1] = temp;
-                }
-            }
-        }
-    }
+	public void orderByIdAsc() {
+		for (int i = 0; i < items.length - 1; i++) {
+			for (int j = 0; j < items.length - i - 1; j++) {
+				if (items[j].getId() > items[j + 1].getId()) {
+					Model temp = items[j];
+					items[j] = items[j + 1];
+					items[j + 1] = temp;
+				}
+			}
+		}
+	}
 
-    public void orderByIdDesc() {
-        for (int i = 0; i < items.length - 1; i++) {
-            for (int j = 0; j < items.length - i - 1; j++) {
-                if (items[j].getId() < items[j + 1].getId()) {
-                    Model temp = items[j];
-                    items[j] = items[j + 1];
-                    items[j + 1] = temp;
-                }
-            }
-        }
-    }
+	public void orderByIdDesc() {
+		for (int i = 0; i < items.length - 1; i++) {
+			for (int j = 0; j < items.length - i - 1; j++) {
+				if (items[j].getId() < items[j + 1].getId()) {
+					Model temp = items[j];
+					items[j] = items[j + 1];
+					items[j + 1] = temp;
+				}
+			}
+		}
+	}
 
-    public boolean update(int position, Model model) {
-        if (items[position] != null) {
-            items[position] = model;
-            return true;
-        }
+	public boolean update(int position, Model model) {
+		if (items[position] != null) {
+			items[position] = model;
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 }
--- src/main/java/vitor/dev/array/PersonArray.java
+++ src/main/java/vitor/dev/array/PersonArray.java
@@ -4,43 +4,41 @@ import vitor.dev.model.Model;
 import vitor.dev.model.Person;
 
 public class PersonArray extends MyArray {
-    public PersonArray(int size) {
-        super(size);
-    }
+	public PersonArray(int size) {
+		super(size);
+	}
 
-    @Override
-    public void insert(int position, Model model) {
-        if (model instanceof Person) {
-            super.insert(position, model);
-        } else {
-            throw new IllegalArgumentException();
-        }
-    }
+	@Override
+	public void insert(int position, Model model) {
+		if (model instanceof Person) {
+			super.insert(position, model);
+		}
+	}
 
-    @Override
-    public boolean remove(Model model) {
-        if (model instanceof Person) {
-            return super.remove(model);
-        } else {
-            throw new IllegalArgumentException();
-        }
-    }
+	@Override
+	public boolean remove(Model model) {
+		if (model instanceof Person) {
+			return super.remove(model);
+		}
 
-    @Override
-    public boolean search(Model model) {
-        if (model instanceof Person) {
-            return super.search(model);
-        } else {
-            throw new IllegalArgumentException();
-        }
-    }
+		return false;
+	}
 
-    @Override
-    public boolean update(int position, Model model) {
-        if (model instanceof Person) {
-            return super.update(position, model);
-        } else {
-            throw new IllegalArgumentException();
-        }
-    }
+	@Override
+	public boolean search(Model model) {
+		if (model instanceof Person) {
+			return super.search(model);
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean update(int position, Model model) {
+		if (model instanceof Person) {
+			return super.update(position, model);
+		}
+
+		return false;
+	}
 }
