a3e6a907d30d683e0140827fbfeb4407dba19b87
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-30 15:39:35 -0300

    feat: equals na classe model

--- src/main/java/vitor/dev/model/Model.java
+++ src/main/java/vitor/dev/model/Model.java
@@ -1,13 +1,19 @@
 package vitor.dev.model;
 
 public class Model {
-    private int id;
+	private int id;
 
-    public Model(int id) {
-        this.id = id;
-    }
+	public Model(int id) {
+		this.id = id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Model other = (Model) obj;
+		return id == other.id;
+	}
 }
