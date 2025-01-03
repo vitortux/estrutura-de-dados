f45e0aa5f697f46bd8ddfec9376d9c376c31cd22
vito-souza <vitorlegalvh@gmail.com>
2025-01-02 22:13:53 -0300

    refactor: métodos hashcode e equals na classe model

--- src/main/java/vitor/dev/model/Model.java
+++ src/main/java/vitor/dev/model/Model.java
@@ -1,7 +1,12 @@
 package vitor.dev.model;
 
+import java.util.Objects;
+
 public class Model {
 	private int id;
+
+	public Model() {
+	}
 
 	public Model(int id) {
 		this.id = id;
@@ -11,8 +16,26 @@ public class Model {
 		return id;
 	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
 	@Override
 	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
 		Model other = (Model) obj;
 		return id == other.id;
 	}
